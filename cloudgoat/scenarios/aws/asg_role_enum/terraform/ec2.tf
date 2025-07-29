038d5693663a1e8bd610bf30e3660d206b735b79
jihyang <hyanglee0508@naver.com>
2025-07-30 04:22:29 +0900

    add13

--- cloudgoat/scenarios/aws/asg_role_enum/terraform/ec2.tf
+++ cloudgoat/scenarios/aws/asg_role_enum/terraform/ec2.tf
@@ -1,7 +1,7 @@
 data "aws_instances" "launched_by_asg" {
   filter {
     name   = "tag:Name"
-    values = ["Privileged1-LaunchTemplate"]
+    values = ["cg-whs-fronted-asg-ec2"]
   }
 
   filter {
@@ -10,4 +10,4 @@ data "aws_instances" "launched_by_asg" {
   }
 
   depends_on = [aws_autoscaling_group.whs_fronted_asg]
-} 
+}
